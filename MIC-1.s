8c99c557fcc850ccc754e79713c1ae93f8fda613
MichaelH13 <mhiggins13@georgefox.edu>
2015-12-03 13:28:26 -0800

    Decoding instructions should be working

--- MIC-1.s
+++ MIC-1.s
@@ -19,12 +19,25 @@ readResult: .asciz "Read in: %d\n"
 flags: .asciz "r"
 
 .balign 4
-data: .skip 4096
-
-.balign 4
-data_size: .word 0
+memory: .skip 4096
 
 .text
+
+.macro _WR_
+    ldr r1, =memory
+    str mic1MDR, [r1, +mic1MAR]
+.endm
+
+.macro _RD_
+    ldr r1, =memory
+    ldr mic1MDR, [r1, +mic1MAR]
+.endm
+
+.macro _INC_PC_FETCH
+    add mic1PC, #1
+    ldr r0, =memory
+    add r0, mic1PC
+.endm
 
     /* Naming Registers */
     /* r2 is MAR    : Memory Address Register. */
@@ -101,19 +114,19 @@ main:
  	bl printf           /* call printf */
  	mov r0, r4          /* put FILE back into r0 */
  	
- 	ldr r0, =data       /* store pointer to data in r0 */
+ 	ldr r0, =memory       /* store pointer to memory in r0 */
  	mov r1, #1          /* store 1 in r1 (read chunks of 1 byte) */
  	ldr r2, =4096       /* store 8 in r2 (read 8 chunks of 1 byte) */
  	mov r3, r4          /* store FILE in r3, read from FILE */
- 	bl fread            /* read 8 bytes from FILE into data array */
+ 	bl fread            /* read 8 bytes from FILE into memory array */
  	
  	mov r7, r0          /* store # of bytes read in at r7, SP essentially */
  	
  	mov r6, #0          /* zero-out our counter */
     
- printData:
+ printMemory:
     /* Print using printf */
-    /*ldr r1, =data
+    /*ldr r1, =memory
     add r1, r6
     ldrb r1, [r1]
     
@@ -121,7 +134,7 @@ main:
     bl printf
     add r6, #1
     cmp r6, r7
-    bllt printData*/
+    bllt printMemory*/
  	
  	/* Make sure we are starting at 0 */
  	mov mic1PC, #0
@@ -130,7 +143,7 @@ main:
  	
  main1:
     /* load first byte */
-    /*ldr r1, =data
+    /*ldr r1, =memory
     add r1, #2
     add r1, mic1PC
     ldrb r1, [r1]
@@ -140,30 +153,62 @@ main:
     /**/
     
     /* fetch byte instruction. */
-    ldr r0, =data
+    ldr r0, =memory
     add r0, mic1PC
     mov mic1MAR, r0
     ldrb mic1MBR, [mic1MAR]
     
-    
-    
     /* decode/execute */
     cmp mic1MBR, #0x10
     beq bipush
+    cmp mic1MBR, #0x59
+    beq dup
+    cmp mic1MBR, #0xA7
+    beq goto
     cmp mic1MBR, #0x60
     beq iadd
     cmp mic1MBR, #0x00
     beq nop
     cmp mic1MBR, #0xA9
     beq ret
+    cmp mic1MBR, #0x7E
+    beq iand
+    cmp mic1MBR, #0x99
+    beq ifeq
+    cmp mic1MBR, #0x9B
+    beq iflt
+    cmp mic1MBR, #0x9F
+    beq if_icmpeq
+    cmp mic1MBR, #0x84
+    beq iinc
+    cmp mic1MBR, #0x15
+    beq iload
+    cmp mic1MBR, #0xA8
+    beq jsr
+    cmp mic1MBR, #0x80
+    beq ior
+    cmp mic1MBR, #0x36
+    beq istore
+    cmp mic1MBR, #0x64
+    beq isub
+    cmp mic1MBR, #0x57
+    beq pop
+    cmp mic1MBR, #0x5F
+    beq swap
  
- exit:
+ end:
  	pop {lr}
-
+ 	/* print TOS, then exit
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf */
 	bx lr
 	
 bipush:
-    ldr r1, =data
+    ldr r1, =memory
     add r1, mic1PC
     ldrb r1, [r1]
     
@@ -172,7 +217,18 @@ bipush:
     
     add mic1PC, #1
     
-    ldr r1, =data
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    b main1
+    
+dup:
+    ldr r1, =memory
     add r1, mic1PC
     ldrb r1, [r1]
     
@@ -182,8 +238,49 @@ bipush:
     add mic1PC, #1
     b main1
 
+goto:
+    /*print goto*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    /*offset*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    b main1
+    
 iadd:
-    ldr r1, =data
+     sub mic1SP, mic1SP, #4        @MAR = SP = SP - 1
+     mov mic1MAR, mic1SP
+     mov mic1H, mic1TOS            @mic1H = TOS
+     _RD_                          @TOS = MDR + H
+     add mic1TOS, mic1MDR, mic1H
+     mov mic1MDR, mic1TOS          @ MDR = TOS
+     _WR_
+     b main1
+
+iand:
+    ldr r1, =memory
     add r1, mic1PC
     ldrb r1, [r1]
     
@@ -191,9 +288,257 @@ iadd:
     bl printf
     add mic1PC, #1
     b main1
+    
+ifeq:
+    /*print ifeq*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    /*offset*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    b main1
+    
+iflt:
+    /*print iflt*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    /*print offset*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    b main1
+    
+if_icmpeq:
+    /*print if_icmpeq*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    /*print offset*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    b main1   
+    
+iinc:
+    /*print iinc*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    /*print varnum*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    /*print const*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    b main1
+    
+iload:
+    /*print iload*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    /*print varnum*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
 
+    b main1
+    
+jsr:
+    /*print jsr*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    /*print disp*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    b main1
+
+ior:
+    /*print ior*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    add mic1PC, #1
+    b main1
+    
+istore:
+    /*print istore*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+    
+    /*print varnum*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    
+    add mic1PC, #1
+
+    b main1
+    
+isub:
+    /*print isub*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    add mic1PC, #1
+    b main1
+    
 nop:
-    ldr r1, =data
+    /*print nop*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    add mic1PC, #1
+    b main1
+    
+pop:
+    /*print pop*/
+    ldr r1, =memory
+    add r1, mic1PC
+    ldrb r1, [r1]
+    
+    ldr r0, =here
+    bl printf
+    add mic1PC, #1
+    b main1
+    
+swap:
+    /*print swap*/
+    ldr r1, =memory
     add r1, mic1PC
     ldrb r1, [r1]
     
@@ -203,13 +548,13 @@ nop:
     b main1
 
 ret:
-    ldr r1, =data
+    ldr r1, =memory
     add r1, mic1PC
     ldrb r1, [r1]
     
     ldr r0, =here
     bl printf
-    b exit
+    b end
 	
 	/* Open that file and read it byte-by-byte into an array */
 	/* of bytes that will represent our memory. */
